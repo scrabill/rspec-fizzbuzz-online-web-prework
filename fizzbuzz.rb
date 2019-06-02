1a4f4b05cf87ab210e95188f5577c000f7d2624f
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-02 23:15:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,7 +6,7 @@ def fizzbuzz(int)
   if int % 5 == 0 && int % 3 == 0
   "FizzBuzz"
 
-  if int % 3 == 0 # if the number int is divisible by 3
+  elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
 
   elsif int % 5 == 0
